146e96804f021a37319a887a7f2ae80177349fab
João Vitor <jvitordiass@outlook.com>
2024-02-08 17:39:57 -0300

    feat: add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,16 @@
 export type TProduto = {
   nf: string
-  produto: string
+  nome: string
+}
+
+export type TDadosDoProduto = {
+  id: string
+  nome: string
+  barra: string
+  barraXML: string
+}
+
+export type TNF = {
+  descricao: string
+  codigo: string
 }
