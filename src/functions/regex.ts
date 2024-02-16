e3ce2b166b011c3670a2f6ab2bb8ee5860fb9132
João Vitor <jvitordiass@outlook.com>
2024-02-16 15:53:11 -0300

    fix: change estoque url

--- src/functions/regex.ts
+++ src/functions/regex.ts
@@ -2,7 +2,7 @@ export function obterEstoqueURL(html: string) {
   const resultado = html.match(/(?<=href=")https:\/\/www.sambanet.net.br\/sambanet\/estoque.+(?===".+demo)/)
   if (!resultado) return null
   const [url] = resultado
-  return url + '=='
+  return (url + '==').replace('estoque2', 'estoque')
 }
 
 export function obterEntradaNfURL(html: string) {
