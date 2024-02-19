03a9df0ef2e7596b7d33cbc91a43efc9156662bf
João Vitor <jvitordiass@outlook.com>
2024-02-19 09:42:37 -0300

    refactor: remove unused imports

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,6 +1,3 @@
-import fs, { promises as pfs } from 'fs'
-import { TProduto } from '../types'
-
 export async function delay(value?: number) {
   await new Promise(r => setTimeout(r, value || 5000))
 }
