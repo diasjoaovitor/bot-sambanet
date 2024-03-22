a0b529e9a651b949ea4e80dbf718e38dfd78eae6
João Vitor <jvitordiass@outlook.com>
2024-03-22 17:31:31 -0300

    chore: add new rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-floating-promises': 'off'
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/return-await': 'off'
   }
 }
