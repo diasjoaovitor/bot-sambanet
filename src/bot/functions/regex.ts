f70bbb0141ca2b4c8539290e5a0a48132f69e2b9
João Vitor <jvitordiass@outlook.com>
2024-09-05 16:40:34 -0300

    refactor: change estoque url

--- src/bot/functions/regex.ts
+++ src/bot/functions/regex.ts
@@ -4,13 +4,13 @@ export function getEstoqueURL(html: string) {
   )
   if (!result) return null
   const [url] = result
-  return (url + '==').replace('estoque2', 'estoque')
+  return url + '=='
 }
 
 export function getEntradaNfURL(html: string) {
   const result = html.match(/(?<=href=")EntradaNfRM.+(?=" title.+tabindex)/)
   if (!result) return null
   const [href] = result
-  const url = 'https://www.sambanet.net.br/sambanet/estoque/Forms/' + href
+  const url = 'https://www.sambanet.net.br/sambanet/estoque3/Forms/' + href
   return url
 }
