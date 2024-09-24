511db8762e3af0df1566996a4b8ddbe6a448ad35
João Vitor <jvitordiass@outlook.com>
2024-09-24 11:30:25 -0300

    fix: change estoque url

--- src/bot/functions/regex.ts
+++ src/bot/functions/regex.ts
@@ -11,6 +11,6 @@ export function getEntradaNfURL(html: string) {
   const result = html.match(/(?<=href=")EntradaNfRM.+(?=" title.+tabindex)/)
   if (!result) return null
   const [href] = result
-  const url = 'https://www.sambanet.net.br/sambanet/estoque3/Forms/' + href
+  const url = 'https://www.sambanet.net.br/sambanet/estoque/Forms/' + href
   return url
 }
